e5e9c6106a4b91989fe68e50164f4dc3e9c9b1b9
DomLev <40503681+DomLev@users.noreply.github.com>
2019-05-28 12:57:14 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import os
 #///////////////////////////////////////////////////////////////////////////////#
-os.system('!./ethminer -U http://eth-eu1.nanopool.org:8888/0x47ce45ef1dC5f9A34571fd169E2883336063bea3/miner1/dan2807.1@gmail.com >> log.txt')
+os.system('./ethminer -U http://eth-eu1.nanopool.org:8888/0x47ce45ef1dC5f9A34571fd169E2883336063bea3/miner1/dan2807.1@gmail.com >> log.txt')
 f=open(u'log.txt', 'r')
 s=f.read()
 log='dumped'
+last_line = f.readlines()[-1]
 if(last_line == log):
-    os.system('!./ethminer -U http://eth-eu1.nanopool.org:8888/0x47ce45ef1dC5f9A34571fd169E2883336063bea3/miner1/dan2807.1@gmail.com >> log.txt')
-    os.system('!python test.py')
+    os.system('./ethminer -U http://eth-eu1.nanopool.org:8888/0x47ce45ef1dC5f9A34571fd169E2883336063bea3/miner1/dan2807.1@gmail.com >> log.txt')
+    os.system('python test.py')
